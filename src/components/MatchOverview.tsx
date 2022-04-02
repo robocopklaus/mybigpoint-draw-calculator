faf87484deb6164ade262c967c47644e19ee02b7
Fabian Pahl <fabian@21st.digital>
2022-04-02 16:28:24 +0200

    feat: added draw calculation

---
+++ src/components/MatchOverview.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+import { Match, Player } from '../types'
+
+interface MatchOverviewProps {
+  matches: Match[]
+  player?: Player
+}
+
+const MatchOverview = ({ matches, player }: MatchOverviewProps): JSX.Element => {
+  return (
+    <table className="w-full text-sm text-left text-neutral-500 dark:text-neutral-400">
+      <thead className="text-xs uppercase text-neutral-700 bg-neutral-50 dark:bg-neutral-700 dark:text-neutral-400">
+        <tr>
+          <th scope="col" className="px-6 py-3">
+            Name
+          </th>
+          <th scope="col" className="px-6 py-3">
+            LK
+          </th>
+          <th scope="col" className="px-6 py-3">
+            Name
+          </th>
+          <th scope="col" className="px-6 py-3">
+            LK
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {matches.map((match, index) => (
+          <tr
+            key={index}
+            className={`${
+              match.playerOne.name === player?.name || match.playerTwo.name === player?.name
+                ? 'bg-green-400 dark:bg-green-900 hover:bg-green-300 dark:hover:bg-green-800'
+                : 'bg-white dark:bg-neutral-800 hover:bg-neutral-50 dark:hover:bg-neutral-600'
+            } border-b dark:border-neutral-700 `}
+          >
+            <td className="px-6 py-4 font-medium text-neutral-900 dark:text-white whitespace-nowrap">
+              {match.playerOne.name}
+            </td>
+            <td className="px-6 py-4">{match.playerOne.rating}</td>
+            <td className="px-6 py-4 font-medium text-neutral-900 dark:text-white whitespace-nowrap">
+              {match.playerTwo.name}
+            </td>
+            <td className="px-6 py-4">{match.playerTwo.rating}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+export default MatchOverview
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,12 +1,78 @@
 import './styles.css'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
+import browser from 'webextension-polyfill'
+
+import MatchOverview from './components/MatchOverview'
+import { Match, Player } from './types'
+import { generateMatches } from './utils'
 
 const Popup = (): JSX.Element | null => {
+  const [matches, setMatches] = useState<Match[]>([])
+  const [players, setPlayers] = useState<Player[]>([])
+  const [currentPlayer, setCurrentPlayer] = useState<Player>()
+
+  const getPlayers = async (): Promise<Player[]> => {
+    const [tab] = await browser.tabs.query({ active: true, currentWindow: true })
+    if (tab?.id == null) return []
+
+    // TODO: Define message type
+    const players = await browser.tabs.sendMessage(tab.id, { action: 'PARSE_PLAYERS' })
+    return players
+  }
+
+  useEffect(() => {
+    getPlayers()
+      .then(players => {
+        setPlayers(players)
+        if (players.length >= 3) {
+          setMatches(generateMatches(players))
+        }
+      })
+      .catch(console.error)
+
+    browser.storage.sync
+      .get('player')
+      .then(({ player }) => {
+        setCurrentPlayer(player)
+      })
+      .catch(console.error)
+  }, [setMatches, setPlayers, setCurrentPlayer])
+
+  const handleClick = async (): Promise<void> => {
+    if (currentPlayer == null) {
+      await browser.runtime.openOptionsPage()
+    } else {
+      setPlayers([...players, currentPlayer])
+      setMatches(generateMatches([...players, currentPlayer]))
+    }
+  }
+
+  const NotEnoughPlayers = (): JSX.Element => (
+    <span className="font-bold py-4 px-2">Nicht genügend Spieler angemeldet</span>
+  )
+
+  const isAddMeEnabled = (): boolean => players.findIndex(player => player.name === currentPlayer?.name) === -1
+
   return (
     <div className="w-max p-4 min-h-screen dark:bg-neutral-800">
-      <h1>mybigpoint Draw Calculator</h1>
+      {players.length < 3 ? (
+        <NotEnoughPlayers />
+      ) : (
+        <>
+          <MatchOverview matches={matches} player={currentPlayer} />
+
+          {isAddMeEnabled() ? (
+            <button
+              className="mt-4 py-2 w-full font-semibold text-sm border-neutral-800 bg-neutral-900 rounded text-neutral-100 dark:border-neutral-100 dark:bg-neutral-50 dark:text-neutral-900"
+              onClick={handleClick}
+            >
+              Mich zum Feld hinzufügen
+            </button>
+          ) : null}
+        </>
+      )}
     </div>
   )
 }
